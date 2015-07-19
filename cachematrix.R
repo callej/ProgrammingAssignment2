f433f0b2b785048d7b4f020965f0b19c5d5411a4
callej <callej@live.com>
2015-07-19 14:38:46 +0200

    Coursera R Programming Week 3 - Caching the Inverse of a Matrix
    2 functions created:
    1. makeCacheMatrix() to create a matrix object and handle access to the
    matrix and its inverse.
    2. cacheSolve() to retrieve the inverse of the matrix in the created
    object.
    
    cacheSolve uses a cached copy of the inverse if it exists. Otherwise it
    calculates the inverse, stores it in the matrix object and returns the
    inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The 2 functions provide functionality to cache the inverse of
+## a given matrix and reuse it later. Calculating the inverse is
+## only done if it hasn't been done before for the given martix.
 
-## Write a short comment describing this function
+## Usage:   Let A be an invertable matrix
+##          > m <- makeCacheMatrix(A)       ## Creates a matrix object.
+##          > cacheSolve(m)                 ## Retrieves the inverse of
+                                            ## the matrix in m.
+
+
+
+## The function makeCacheMatrix returns a list of functions
+## to set a matrix and its inverse as well as accessing them.
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL           ## The inverse matrix initializes to NULL
+  
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL      ## The inverse matrix in set to NULL when 
+    }                     ## a new matrix is set
 
+  
+    get <- function() x
+  
+    setinv <- function(x_inverse) inv <<- x_inverse
+  
+    getinv <- function() inv
+  
+    list(set = set, get = get,    ## makeCacheMatrix returns a list
+         setinv = setinv,         ## of the 4 functions defined.
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+## The function cacheSolve returns the inverse of a given matrix
+## that was created using the makeCacheMatrix function. The function
+## returns the cached version of the matrix if it exists. Otherwise
+## it calculates the inverse, adds it to the cache and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    mat <- x$get()
+    inv <- solve(mat, ...)
+    x$setinv(inv)
+    inv
 }
